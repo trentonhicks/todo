121670e7f88bccaf97e09956f4f1b0d86c454a30
Trenton <trentonmhicks@gmail.com>
2020-03-14 13:48:02 -0500

    Created starting point for Accounts controller.

---
+++ TodoWebAPI/Controllers/AccountsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TodoWebAPI.Models;
+
+namespace TodoWebAPI.Controllers
+{
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+        private readonly IConfiguration _config;
+
+        public AccountsController(ToDoContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpPost("accounts")]
+        public IActionResult CreateAccount()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("accounts/{id}")]
+        public IActionResult GetAccount()
+        {
+            return NotFound();
+        }
+    }
+}
