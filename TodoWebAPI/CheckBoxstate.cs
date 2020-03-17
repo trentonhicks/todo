0afcc36b376c26268113b2ab7b001bea17deeb59
Daniel Figueroa <Blackspade123@hotmail.com>
2020-03-17 17:27:02 -0500

    Added a class for the check box status. Finished or unfinished.

---
+++ TodoWebAPI/CheckBoxstate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoWebAPI
+{
+    public static class CheckBoxstate
+    {
+        public static bool Finished = true;
+        public static bool Unfinished = false;
+
+        public static string ConvertToString(bool state)
+        {
+            string stringValue;
+
+            switch (state)
+            {
+                default:
+                    stringValue = "Unfinished";
+                    break;
+                case true:
+                    stringValue = "Finished";
+                    break;
+                case false:
+                    stringValue = "Unfinished";
+                    break;
+            }
+            return stringValue;
+        }
+    }
+}
