8128b787dfd1fde622da991052342e07945289ce
Trenton <trentonmhicks@gmail.com>
2020-03-18 17:07:03 -0500

    Finished CreateListAsync for in memory code.

---
+++ TodoWebAPI/Presentation/CreateListPresentation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoWebAPI.Presentation
+{
+    public class CreateListPresentation
+    {
+        public int Id { get; set; }
+        public string ListTitle { get; set; }
+    }
+}
