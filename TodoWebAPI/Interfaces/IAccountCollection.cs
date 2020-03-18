e24d81cceb1f9cdb6f244f82e88d9e2b2ca027d6
plwieseler@gmail.com <plwieseler@gmail.com>
2020-03-18 09:54:14 -0500

    Created an interface for Accounts.

---
+++ TodoWebAPI/Interfaces/IAccountCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoWebAPI.Data;
+
+namespace TodoWebAPI.Interfaces
+{
+    public interface IAccountCollection
+    {
+        Accounts GetAccount(Accounts account);
+        Accounts CreateAccount(Accounts account);
+        void DeleteAccounts(Accounts account);
+    }
+}
