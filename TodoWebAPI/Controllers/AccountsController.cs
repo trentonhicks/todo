94751cb14f4d0908cd6771655959d8bf26ee9991
Daniel Figueroa <Blackspade123@hotmail.com>
2020-03-19 00:32:13 -0500

    ToDo post, put and delete work with InMemoryTodo

--- TodoWebAPI/Controllers/AccountsController.cs
+++ TodoWebAPI/Controllers/AccountsController.cs
@@ -23,6 +23,8 @@ namespace TodoWebAPI.Controllers
 
         private IAccountRepository _account;
         private IListsRepository _lists = new InMemoryListsRepository();
+        private IToDoRepository _todo = new InMemoryToDo();
+
 
         public AccountsController(ToDoContext context, IConfiguration config)
         {
@@ -35,12 +37,12 @@ namespace TodoWebAPI.Controllers
         [HttpPost("accounts")]
         public async Task<IActionResult> CreateAccount(CreateAccountModel accountToCreate)
         {
-            
-            if(accountToCreate.UserName == null)
+
+            if (accountToCreate.UserName == null)
             {
                 return BadRequest("Username required");
             }
-            else if(accountToCreate.Password == null)
+            else if (accountToCreate.Password == null)
             {
                 return BadRequest("Password required");
             }
@@ -55,7 +57,7 @@ namespace TodoWebAPI.Controllers
             var accounts = await _account.CreateAccountAsync(account);
 
             return Ok(accounts);
-          
+
         }
 
         [HttpGet("accounts/{accountId}")]
@@ -63,7 +65,7 @@ namespace TodoWebAPI.Controllers
         {
             var account = await _account.GetAccountAsync(accountId);
 
-            if(account == null)
+            if (account == null)
             {
                 return BadRequest("Profile Doesn't exist");
             }
@@ -75,14 +77,14 @@ namespace TodoWebAPI.Controllers
                 UserName = account.UserName,
                 Picture = account.Picture
             };
-          
+
             return Ok(accountPresentation);
         }
 
         [HttpDelete("accounts/{accountId}")]
         public IActionResult DeleteAccount(int accountId)
         {
-             _account.DeleteAccountsAsync(accountId);
+            _account.DeleteAccountsAsync(accountId);
             return Ok("Acccount Deleted");
         }
 
@@ -203,49 +205,95 @@ namespace TodoWebAPI.Controllers
         }
 
         [HttpPost("accounts/{accountId}/lists/{listId}/todos")]
-        public IActionResult CreateTodo(int accountId, int listId, [FromBody] ToDos todos)
+        public async Task<IActionResult> CreateTodo(int accountId, int listId, [FromBody] CreateToDoModel todos)
         {
-            var list = _context.Lists.Find(listId);
-
-            todos.ListId = listId; 
-
-            if (list != null)
+            var todo = new ToDos()
             {
-                if (list.AccountId != accountId)
-                {
-                    return BadRequest("List belongs to another account");
-                }
-                _context.ToDos.Add(todos);
-                _context.SaveChanges();
-                return Ok();
-            }
-            return NotFound("List doesn't exist.");
+                ToDoName = todos.ToDoName,
+                ParentId = todos.ParentId,
+                Notes = todos.Notes,
+                Completed = todos.Completed,
+                ListId = listId
+            };
+            var foo = await _todo.CreateToDoAsync(todo);
+
+            return Ok(foo);
         }
 
+        //var list = _context.Lists.Find(listId);
+
+        //todos.ListId = listId; 
+
+        //if (list != null)
+        //{
+        //    if (list.AccountId != accountId)
+        //    {
+        //        return BadRequest("List belongs to another account");
+        //    }
+        //    if (todos.ParentId != null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    _context.ToDos.Add(todos);
+        //    _context.SaveChanges();
+        //    return Ok();
+
+        //return NotFound("List doesn't exist.");
+
         [HttpPut("accounts/{accountId}/todos/{todoId}")]
-        public IActionResult EditTodo(int accountId, int todoId)
+        public async Task<IActionResult> EditTodo(int accountId, int todoId, [FromBody] ToDos todo)
         {
-            return NotFound();
+
+            var foo = await _todo.UpdateToDoAsync(todoId, todo);
+
+            return Ok(foo);
+
+
+            //var todo = _context.ToDos.Find(todoId);
+            //if (_contextService.AccountExists(accountId))
+            //{
+            //    if (_contextService.ToDoExists(todoId))
+            //    {
+            //        var toDoName = todo.ToDoName;
+            //        var toDoNote = todo.Notes;
+            //        var toDoState = todo.Completed;
+            //        if (toDoName != null)
+            //        {
+            //            _context.ToDos.Update(todo);
+            //            _context.SaveChanges();
+            //            return Ok();
+            //        }
+            //    }
+
+            //    //logic to update the title
+            //    //logic to update the note
+            //    //logic to update the state
+            //}
+            //return NotFound();
         }
 
         [HttpDelete("accounts/{accountId}/todos/{todoId}")]
-        public IActionResult DeleteTodo(int accountId, int todoId)
+        public async Task<IActionResult> DeleteTodo(int accountId, int todoId)
         {
-            var todo = _context.ToDos.Find(todoId);
-            if (_contextService.AccountExists(accountId))
-            {
-                if (todo != null)
-                {
-                    _context.ToDos.Remove(todo);
-                    _context.SaveChanges();
-                    return Ok("ToDo list removed.");
-                }
-                else
-                {
-                    return NotFound("ToDo is already empty");
-                }
-            }
-            return NotFound("Account doesn't exist.");
+            await _todo.DeleteToDoAsync(todoId);
+
+            return Ok();
+
+            //var todo = _context.ToDos.Find(todoId);
+            //if (_contextService.AccountExists(accountId))
+            //{
+            //    if (todo != null)
+            //    {
+            //        _context.ToDos.Remove(todo);
+            //        _context.SaveChanges();
+            //        return Ok("ToDo list removed.");
+            //    }
+            //    else
+            //    {
+            //        return NotFound("ToDo is already empty");
+            //    }
+            //}
+            //return NotFound("Account doesn't exist.");
         }
     }
 }
