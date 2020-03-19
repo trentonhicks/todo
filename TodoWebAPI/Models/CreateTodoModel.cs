ecab3d5032eb9a1c001906a3b4de7bf774257781
Daniel Figueroa <Blackspade123@hotmail.com>
2020-03-18 23:53:53 -0500

    added a model for creating a todo

---
+++ TodoWebAPI/Models/CreateTodoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoWebAPI.Models
+{
+    public class CreateToDoModel
+    {
+        public int? ParentId { get; set; }
+        public string Notes { get; set; }
+        public bool Completed { get; set; }
+        public string ToDoName { get; set; }
+    }
+}
