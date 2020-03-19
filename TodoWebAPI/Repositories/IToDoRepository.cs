e87a8c5dfe2eec3e8a4851c3101c546b8e9e16ec
Daniel Figueroa <Blackspade123@hotmail.com>
2020-03-18 23:34:13 -0500

    Created an interface for todo items

---
+++ TodoWebAPI/Repositories/IToDoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoWebAPI.Data;
+using TodoWebAPI.Models;
+
+namespace TodoWebAPI.Repositories
+{
+    public interface IToDoRepository
+    {
+        Task<ToDos> CreateToDoAsync(ToDos toDo);
+        Task<ToDos> UpdateToDoAsync(int listId, ToDos toDo);
+        Task DeleteToDoAsync(int todo);
+    }
+}
