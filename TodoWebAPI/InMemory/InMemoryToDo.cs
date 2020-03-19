cd06806187ef561528163a2d48d90a5a94972d02
Daniel Figueroa <Blackspade123@hotmail.com>
2020-03-19 00:15:35 -0500

    Made some changes to the InMemoryToDo class

--- TodoWebAPI/InMemory/InMemoryToDo.cs
+++ TodoWebAPI/InMemory/InMemoryToDo.cs
@@ -12,16 +12,9 @@ namespace TodoWebAPI.InMemory
     {
         public InMemoryToDo()
         {
-            _accounts = new List<AccountModel>();
-            _accounts.Add(new AccountModel() { Id = 1, FullName = "Parker", UserName = "parker", Picture = "", Password = "1234" });
-            _list = new List<ListModel>();
-            _list.Add(new ListModel() { Id = 1, AccountId = 1, ListTitle = "New List" });
-
             _todo = new List<ToDos>();
-            _todo.Add(new ToDos() { Id = 1, Completed = false, ListId = 1, Notes = "", ToDoName = "yes" });
+            _todo.Add(new ToDos() { Id = 1, Completed = false, ListId = 1, Notes = "dude", ToDoName = "Clean" });
         }
-        private List<AccountModel> _accounts;
-        private List<ListModel> _list;
         private List<ToDos> _todo;
 
         public Task<ToDos> CreateToDoAsync(ToDos toDo)
@@ -30,6 +23,7 @@ namespace TodoWebAPI.InMemory
             _todo.Add(toDo);
             return Task.FromResult(toDo);
         }
+        
         public Task<ToDos> UpdateToDoAsync(int todoId, ToDos todo)
         {
             var foo = new ToDos()
@@ -42,15 +36,7 @@ namespace TodoWebAPI.InMemory
 
             return Task.FromResult(foo);
         }
-        //public Task<string> UpdateListAsync(int listId, string title)
-        //{
-        //    var listIndex = _lists.FindIndex(x => x.Id == listId);
-        //    _lists[listIndex].ListTitle = title;
-
-        //    return Task.FromResult(title);
-        //}
-
-
+        
         public async Task DeleteToDoAsync(int todoId)
         {
             var todo = _todo.Find(x => x.Id == todoId);
