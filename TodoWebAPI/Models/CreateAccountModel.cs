fee4d68f51b8de66c3aee9925a0edfd2f6b005e3
plwieseler@gmail.com <plwieseler@gmail.com>
2020-03-23 21:25:25 -0500

    Updated CreateAccount Model.

--- TodoWebAPI/Models/CreateAccountModel.cs
+++ TodoWebAPI/Models/CreateAccountModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,11 @@ namespace TodoWebAPI.Models
 {
     public class CreateAccountModel
     {
-        public int Id { get; set; }
         public string FullName { get; set; }
+        [Required(ErrorMessage ="UserName Required")]
         public string UserName { get; set; }
         public string Picture { get; set; }
+        [Required(ErrorMessage = "Password Required")]
         public string Password { get; set; }
     }
 }
