02fb6a8389384ae9c7252f794f0af371f0f4b004
plwieseler@gmail.com <plwieseler@gmail.com>
2020-03-26 11:40:57 -0500

    Implemented send grid email service.

---
+++ TodoWebAPI/Services/SendGridEmailService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoWebAPI.Services
+{
+    public class SendGridEmailService : IEmailService
+    {
+        public async Task SendEmailAsync(Email email)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SendGridApiKey");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(email.From);
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var plainTextContent = email.Body;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, null);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
