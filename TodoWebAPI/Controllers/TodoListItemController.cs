c04d759a7d3c6124d4ed12c2af4b2832944b55e0
plwieseler@gmail.com <plwieseler@gmail.com>
2020-04-24 14:21:09 -0500

    Implemented required and string length data annotations for editing an item and creating an item.

--- TodoWebAPI/Controllers/TodoListItemController.cs
+++ TodoWebAPI/Controllers/TodoListItemController.cs
@@ -18,6 +18,7 @@ using TodoWebAPI.UserStories.ItemCompletedState;
 
 namespace TodoWebAPI.Controllers
 {
+    [ApiController]
     public class TodoListItemController : ControllerBase
     {
         private readonly IConfiguration _config;
