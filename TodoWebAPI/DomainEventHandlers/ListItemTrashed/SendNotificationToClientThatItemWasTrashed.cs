b50b8f2e3c0302e32f332e4a1a70c9649e2f33f8
plwieseler@gmail.com <plwieseler@gmail.com>
2020-05-14 10:59:23 -0500

    Added signalR ItemTrashed event and implemented it on the front-end.
    Co-authored-by: Trenton Hicks <trentonmhicks@gmail.com>

--- TodoWebAPI/DomainEventHandlers/ListItemTrashed/SendNotificationToClientThatItemWasTrashed.cs
+++ TodoWebAPI/DomainEventHandlers/ListItemTrashed/SendNotificationToClientThatItemWasTrashed.cs
@@ -6,20 +6,26 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Todo.Domain.DomainEvents;
+using Todo.Domain.Repositories;
 
 namespace TodoWebAPI.DomainEventHandlers.ListItemTrashed
 {
     public class SendNotificationToClientThatItemWasTrashed : INotificationHandler<ItemMovedToTrash>
     {
         private readonly IHubContext<NotificationHub> _hubContext;
+        private readonly ITodoListRepository _todoListRepository;
 
-        public SendNotificationToClientThatItemWasTrashed(IHubContext<NotificationHub> hubContext)
+        public SendNotificationToClientThatItemWasTrashed(IHubContext<NotificationHub> hubContext, ITodoListRepository todoListRepository)
         {
             _hubContext = hubContext;
+            _todoListRepository = todoListRepository;
         }
-        public Task Handle(ItemMovedToTrash notification, CancellationToken cancellationToken)
+        public async Task Handle(ItemMovedToTrash notification, CancellationToken cancellationToken)
         {
-            return _hubContext.Clients.All.SendAsync("ItemDeleted");
+            var todoList = await _todoListRepository.FindTodoListIdByIdAsync(notification.ListId.GetValueOrDefault());
+
+            await _hubContext.Clients.Users(todoList.Contributors).SendAsync("ItemTrashed", notification.ListId, notification.Item);
         }
     }
 }
+
