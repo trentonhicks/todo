ec78552bf1871558b89b7a439bc9e8d9c589e404
plwieseler@gmail.com <plwieseler@gmail.com>
2020-05-14 10:01:06 -0500

    Added Backend for sending a notification when an item is deleted.

---
+++ TodoWebAPI/DomainEventHandlers/ListItemTrashed/SendNotificationToClientThatItemWasTrashed.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Todo.Domain.DomainEvents;
+
+namespace TodoWebAPI.DomainEventHandlers.ListItemTrashed
+{
+    public class SendNotificationToClientThatItemWasTrashed : INotificationHandler<ItemMovedToTrash>
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public SendNotificationToClientThatItemWasTrashed(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        public Task Handle(ItemMovedToTrash notification, CancellationToken cancellationToken)
+        {
+            return _hubContext.Clients.All.SendAsync("ItemDeleted");
+        }
+    }
+}
